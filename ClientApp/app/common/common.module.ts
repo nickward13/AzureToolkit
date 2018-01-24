beda3e2947da493c9d87d1fe20d0c82053fc219a
nickward13 <nickward@microsoft.com>
2018-01-24 15:38:04 +1100

    user login

--- ClientApp/app/common/common.module.ts
+++ ClientApp/app/common/common.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CognitiveService } from './services/cognitive.service';
 import { AzureHttpClient } from './services/azureHttpClient';
+import { UserService } from './services/user.service';
 @NgModule({
-    providers: [AzureHttpClient, CognitiveService]
+    providers: [AzureHttpClient, CognitiveService, UserService]
 })
 export class LocalCommonModule { }
